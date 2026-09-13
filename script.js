cf50b8670882d3875eb02d08e87360e07878e018
meganathan223 <meganathanh16@gmail.com>
2026-09-13 07:44:58 +0530

    added image slider

--- script.js
+++ script.js
@@ -12,6 +12,37 @@ closenav.addEventListener('click', function () {
     navbar.classList.remove('show');
 })
 
-offerBar.addEventListener('click', function () {
+offerBar?.addEventListener('click', function () {
     offerBar.parentElement.style.display = 'none';
 })
+
+// image slider
+const slider = document.querySelector('.slider');
+const sliderItems = document.querySelectorAll('.slider-item');
+const leftBtn = document.querySelector('.btn.left');
+const rightBtn = document.querySelector('.btn.right');
+
+leftBtn.addEventListener('click', prevSlide)
+rightBtn.addEventListener('click', nextSlide)
+
+let currentSlide = 0;
+const slideCount = sliderItems.length;
+
+function slideImage(index) {
+    currentSlide = index;
+    slider.style.transform = `translateX(-${currentSlide * 33.33}%)`;
+}
+
+function nextSlide() {
+    // console.log('nextslide')
+    currentSlide = (currentSlide + 1) % slideCount;
+    slideImage(currentSlide);
+}
+
+function prevSlide() {
+    // console.log('prevslide')
+    currentSlide = (currentSlide - 1) % slideCount;
+    slideImage(currentSlide);
+}
+
+setInterval(nextSlide, 5000);
